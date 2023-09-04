490cb5c00da75c2a8f1322e68c7345990eaf7856
cljiahao <cljiahao@hotmail.com>
2023-09-04 23:43:20 +0800

    Added blog cards and read more page

--- backend/apps/v1/route_blog.py
+++ backend/apps/v1/route_blog.py
@@ -1,11 +1,27 @@
+from db.repository.blog import list_blogs
+from db.repository.blog import retrieve_blog
+from db.session import get_db
 from fastapi import APIRouter
+from fastapi import Depends
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
 
 templates = Jinja2Templates(directory="templates")
 router = APIRouter()
 
 
 @router.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("blog/home.html", {"request": request})
+def home(request: Request, db: Session = Depends(get_db)):
+    blogs = list_blogs(db=db)
+    return templates.TemplateResponse(
+        "blog/home.html", {"request": request, "blogs": blogs}
+    )
+
+
+@router.get("/app/blog/{id}")
+def blog_detail(request: Request, id: int, db: Session = Depends(get_db)):
+    blog = retrieve_blog(id=id, db=db)
+    return templates.TemplateResponse(
+        "blog/detail.html", {"request": request, "blog": blog}
+    )
